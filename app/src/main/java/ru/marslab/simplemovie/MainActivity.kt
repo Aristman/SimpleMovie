1e330d214003902c3be97bff821358381e21c9f2
Sergey Aristov <aristov775@gmail.com>
2022-04-25 18:26:09 +0300

    init commit

---
+++ app/src/main/java/ru/marslab/simplemovie/MainActivity.kt
@@ -0,0 +1,16 @@
+package ru.marslab.simplemovie
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import ru.marslab.simplemovie.ui.theme.SimpleMovieTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SimpleMovieTheme {
+            }
+        }
+    }
+}
